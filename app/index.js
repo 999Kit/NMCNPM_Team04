f192ccd5b2695dfe2a7a2fd36734ac045efbe539
999Kit <98326962+999Kit@users.noreply.github.com>
2023-10-14 21:56:49 +0700

    ayyy

---
+++ app/index.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+const Home = () => {
+    return (
+        <SafeAreaView>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: 'orange' },
+                    headerShadowVisible: false,
+
+                }}
+            />
+        </SafeAreaView>
+    )
+}
+
+export default Home;
